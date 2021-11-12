28801869d0c863e0f6c5900cc49338b185c7637e
mrmamadi <mrmamadi@outlook.com>
2021-11-12 14:29:15 +0200

    initialised repo. added .gitignore and solution to task 0.1

---
+++ task-0.1.java
@@ -0,0 +1,13 @@
+class PrintValues {
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 1;
+		System.out.println(x);
+		System.out.println(y);
+
+		x += 3;
+		y += x;
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
