aa5f35b4171198a28a95dd8fa62403069bcbc824
mrmamadi <mrmamadi@outlook.com>
2021-11-12 15:01:46 +0200

    solution to task 0.2

---
+++ task-0.2.java
@@ -0,0 +1,16 @@
+class Bodmas {
+	public static void main(String[] args) {
+		int x = (1 + 1 * 2);
+		int y = (1 + 1) * 2;
+		int z = 1 + (1 * 2);
+		int a = 1 + 1 * 2 / 2;
+		double b = (1 + 1 * 2) / 2.0;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
